d2daaf3af471e04a77374557e0b97ad7c24ead81
theailurusfulgens <abdurnaveed786@gmail.com>
2022-06-16 18:34:24 -0700

    boilerplate code for swerve module state

---
+++ include/swerve/units/dimensionless.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "swerve/units/base.h"
+
+namespace units {
+using dimensionless::dB_t;
+using dimensionless::dimensionless_t;
+using dimensionless::scalar;
+using dimensionless::scalar_t;
+}  // namespace units
